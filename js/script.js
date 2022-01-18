848f1b6b2ea774986a0bb531f7d54d2b301700c4
lovelyoyrmia <mokalulovelyo@gmail.com>
2022-01-19 01:46:09 +0700

    handle click

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// about section tab
+(() => {
+  const aboutSection = document.querySelector(".about-section");
+  var tabsContainer = document.querySelector(".about-tabs");
+
+  tabsContainer.addEventListener("click", () => {
+    if (
+      event.target.classList.contains("tab-item") &&
+      !event.target.classList.contains("active")
+    ) {
+      const target = event.target.getAttribute("data-target");
+      tabsContainer
+        .querySelector(".active")
+        .classList.remove("outer-shadow", "active");
+      event.target.classList.add("active", "outer-shadow");
+      aboutSection
+        .querySelector(".tab-content.active")
+        .classList.remove("active");
+      aboutSection.querySelector(target).classList.add("active");
+    }
+  });
+})();
